ed8369b920a16aec445fb2c6d5743c797fcdebe0
Kirill Shcherbakov <50148272+CyrilShch@users.noreply.github.com>
2020-06-24 17:36:22 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,2 +1,15 @@
 from main import main
-main(mode=1)
+import os
+import random
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+
+def setup_experiment(title, logdir="./tb"):
+    experiment_name = "{}@{}".format(title, datetime.now().strftime("%d.%m.%Y-%H:%M:%S"))
+    writer = SummaryWriter(log_dir=os.path.join(logdir, experiment_name))
+    
+    return writer, experiment_name
+  
+writer, experiment_name = setup_experiment("EdgeConnect", logdir="./tb")
+
+main(mode=1, writer=writer)
